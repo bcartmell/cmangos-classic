d1d83220eaede04f7a8598db905fe9769a5dd082
Ubuntu <ubuntu@vps-c55e52a7.vps.ovh.ca>
2026-04-29 20:32:47 +0000

    Creates rebuild script and begins building extract_maps script

---
+++ scripts/extract_maps.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rm -rf ~/WoW Vanilla/
+unzip ~/WoW_Vanilla.zip
---
+++ scripts/rebuild.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+mkdir /home/ubuntu/wow/mangos-classic/build/
+cd /home/ubuntu/wow/mangos-classic/build/
+
+abort() {
+  echo "Encoundered Error: $1"
+  exit 1
+}
+
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mangos-classic/run -DBUILD_EXTRACTORS=ON -DPCH=1 -DDEBUG=0 -DBUILD_PLAYERBOTS=ON -DBUILD_AHBOT=ON || abort "cmake failed"
+make                                                                                                                                || abort "make failed"
+make install                                                                                                                        || abort "make install failed"
