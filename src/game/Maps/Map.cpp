23d5e9abad2e39f68a99eed520d678a69d15578b
Brady Cartmell <bradycartmell@gmail.com>
2026-05-03 21:47:15 -0500

    Fixes typo

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -2556,12 +2556,12 @@ void Map::ScheduleMapReset()
 
     for (Player* player : toEject)
     {
-        
+
         // If the player's hearthstone is on THIS map, send to racial capital instead
         // to avoid looping them back into a map that just reset
         //  -- Why not? lets just try to kick them instead
 
-        play->GetSession()->PlayerLogout()
+        player->GetSession()->PlayerLogout()
 
         /*
          * if (player->m_homebindMapId == GetId())
