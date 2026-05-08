ce875408d960b2c593b5a94a0cf970e3ba730bb0
Brady <brady@abby>
2026-05-08 00:20:12 +0000

    Disables extractors option when recompiling.  We have already extracted the assets from the client

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -8,6 +8,6 @@ abort() {
   exit 1
 }
 
-cmake .. -DCMAKE_INSTALL_PREFIX=/opt/cmangos/run -DBUILD_EXTRACTORS=ON -DPCH=1 -DDEBUG=0 -DBUILD_PLAYERBOTS=ON -DBUILD_AHBOT=ON || abort "cmake failed"
-make -j 4                                                                                                                           || abort "make failed"
-make install                                                                                                                        || abort "make install failed"
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/cmangos/run -DBUILD_EXTRACTORS=OFF -DPCH=1 -DDEBUG=0 -DBUILD_PLAYERBOTS=ON -DBUILD_AHBOT=ON || abort "cmake failed"
+make -j 4                                                                                                                       || abort "make failed"
+make install                                                                                                                    || abort "make install failed"
